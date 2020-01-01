3369b5c1e83a2ed9a838a29c6ecce0c788aee318
verils <verils@qq.com>
2020-01-01 17:21:07 +0800

    make log more useful

--- main.go
+++ main.go
@@ -10,13 +10,19 @@ import (
 
 func main() {
 	if os.Getenv(app.MysqlHost) == "" {
-		_ = os.Setenv(app.MysqlHost, "docker.local")
+		defaultMysqlHost := "docker.local"
+		_ = os.Setenv(app.MysqlHost, defaultMysqlHost)
+		log.Printf("[WARN] missing env %s, set default to '%s'", app.MysqlHost, defaultMysqlHost)
 	}
 	if os.Getenv(app.MysqlUsername) == "" {
-		_ = os.Setenv(app.MysqlUsername, "root")
+		defaultMysqlUsername := "root"
+		_ = os.Setenv(app.MysqlUsername, defaultMysqlUsername)
+		log.Printf("[WARN] missing env %s, set default to '%s'", app.MysqlUsername, defaultMysqlUsername)
 	}
 	if os.Getenv(app.MysqlPassword) == "" {
-		_ = os.Setenv(app.MysqlPassword, "")
+		defaultMysqlPassword := ""
+		_ = os.Setenv(app.MysqlPassword, defaultMysqlPassword)
+		log.Printf("[WARN] missing env %s, set default to '%s'", app.MysqlPassword, defaultMysqlPassword)
 	}
 
 	router := mux.NewRouter()
@@ -28,6 +34,6 @@ func main() {
 
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("static/")))
 
-	log.Printf("server started at port: %d", 8080)
+	log.Printf("[INFO] server started at port: %d", 8080)
 	_ = http.ListenAndServe(":8080", router)
 }
